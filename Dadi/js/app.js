22e17475d79d4ecf15e133b304fa25eca1c9bd07
Lucaponz91 <lucapo1991@msn.com>
2022-07-25 16:05:27 +0200

    dadi: add event listener

---
+++ Dadi/js/app.js
@@ -0,0 +1,12 @@
+const yourDice = [1, 2, 3, 4, 5, 6];
+const aiDice = [1, 2, 3, 4, 5, 6]; 
+const submitElement = document.querySelector('input[type="submit"]');
+submitElement.addEventListener('click', function () {
+    let yourRandomDice = yourDice[Math.floor(Math.random()*yourDice.length)];
+    console.log(yourRandomDice);
+    let aiRandomDice = aiDice[Math.floor(Math.random()*aiDice.length)];
+    console.log(aiRandomDice)
+
+
+}
+)
