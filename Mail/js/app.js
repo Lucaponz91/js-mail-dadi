7edcf480d0b37f4581e05483f291348d15c320f3
Lucaponz91 <lucapo1991@msn.com>
2022-07-25 15:11:53 +0200

    done Mail

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -5,15 +5,22 @@ const emailcontainer = ["arseniolupin@gmail.com", "bikermice@gmail.com", "ispett
 console.log(emailcontainer);
 // Chiedi all’utente la sua email,
 let emailInserita = prompt('Inserisci il tuo indirizzo email');
+let epresente;
 for (let i = 0; i < emailcontainer.length; i++) {
     console.log(i);
     const mailcorrente = emailcontainer[i]
     console.log(mailcorrente);
     if (emailInserita === mailcorrente) {
         console.log("Login effettuato");
+        epresente = true
     } else {
         console.log("Non ti sei registrato");
     }
 }
 // stampa un messaggio appropriato sull’esito del controllo.
+if (epresente) {
+    alert("Login avvenuto con successo!")
+} else{
+    alert("Devi prima registrarti")
+}
 // (non usare includes())
